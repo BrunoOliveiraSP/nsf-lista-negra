8e1806d1018c396e61c56430e44729a1c89a2dc0
Bruno de Oliveira <brunooliveira.me@outlook.com>
2020-10-16 13:32:11 -0300

    ok

--- frontend/site/src/services/listaNegraApi.js
+++ frontend/site/src/services/listaNegraApi.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: "http://18.234.165.97:5000"
+    baseURL: "http://54.82.231.72:5000"
 })
 
 export default class ListaNegraApi {
